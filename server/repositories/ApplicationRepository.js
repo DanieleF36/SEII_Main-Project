ef91018bc343e6dc7065323a04ccf8581ab74f16
Simone Colagiovanni <s308455@studenti.polito.it>
2023-11-09 14:02:51 +0100

    Version 2 (output ok but still internal error)

--- server/repositories/ApplicationRepository.js
+++ server/repositories/ApplicationRepository.js
@@ -1,47 +1,56 @@
-'use strict';
+"use strict";
 
-const sqlite = require('sqlite3');
+const sqlite = require("sqlite3");
 
-const db = new sqlite.Database('db.sqlite', (err) => {
-    if(err) throw err;
+const db = new sqlite.Database("db.sqlite", (err) => {
+  if (err) throw err;
 });
 
-exports.listApplication = (id_teacher)=>{
-    console.log("APPLICATION");
-    const sqlTeacher = "SELECT id_student,id_thesis,data,path_cv,status,id_teacher FROM Application WHERE id_teacher=?";
-    return new Promise((resolve, reject)=>{
-        db.all(sqlTeacher, [id_teacher], (err, row)=>{
-            if (err) {
-                reject(err);
-                return;
-            }
-            if(row==undefined) {
-                resolve({error: 'Application not found.'})
-            }
-            else{
-                const application = {
-                    id_student: row.id_student,
-                    id_thesis: row.id_thesis,
-                    data: row.data,
-                    path_cv: row.path_cv,
-                    status: row.status,
-                    id_teacher: row.id_teacher
-                }
-                resolve(application);
-            }
-        });
+exports.listApplication = (id_teacher) => {
+  console.log("APPLICATION");
+  const sqlTeacher = "SELECT * FROM Application WHERE id_teacher=?";
+  return new Promise((resolve, reject) => {
+    db.all(sqlTeacher, [id_teacher], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (rows == undefined) {
+        resolve({ error: "Application not found." });
+      } else {
+        const application = rows.map((a) => ({
+          id_student: a.id_student,
+          id_thesis: a.id_thesis,
+          data: a.data,
+          path_cv: a.path_cv,
+          status: a.status,
+          id_teacher: a.id_teacher,
+        }));
+        resolve(application);
+      }
     });
-}
+  });
+};
 
-exports.accRefApplication = (status,id_teacher,id_application)=>{
-    const sqlTeacher = "UPDATE Application SET status = ? WHERE id_teacher = ? AND id_application = ?";
-    return new Promise((resolve, reject)=>{
-        db.run(sqlTeacher, [status,id_teacher,id_application], (err, row)=>{
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve(this.changes);
-        });
+exports.accRefApplication = (status, id_teacher, id_application) => {
+  const sqlTeacher =
+    "UPDATE Application SET status = ? WHERE id_teacher = ? AND id_application = ?";
+  return new Promise((resolve, reject) => {
+    db.run(sqlTeacher, [status, id_teacher, id_application], (err, row) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (status == null) {
+        resolve({ error: "Missing new status" });
+      }
+      if (id_teacher == null) {
+        resolve({ error: "Teacher_id missing" });
+      }
+      if (id_application == null) {
+        resolve({ error: "Application_id missing" });
+      }
+      resolve(this.changes);
     });
-}
+  });
+};
--- server/services/TeacherService.js
+++ server/services/TeacherService.js
@@ -1,44 +1,48 @@
-'use strict';
+"use strict";
 
-const teacherRepository = require("../repositories/TeacherRepository");
+const applicationRepository = require("../repositories/ApplicationRepository.js");
 
 /**
  * Theacher accept or reject an application
  *
- * accepted bool  
- * id_professor Integer 
- * id_application Integer 
+ * accepted bool
+ * id_professor Integer
+ * id_application Integer
  * no response value expected for this operation
  **/
-exports.accRefApplication = async function(accepted,id_professor,id_application) {
-    try {
-        if(accepted!=undefined, id_professor!=null, id_application!=null) {
-            let res = await ApplicationRepository.accRefApplication(accepted,id_professor,id_application);
-        }
-        return res;
-    } catch(error) {
-        res.status(500).json({error: error.message})
+exports.accRefApplication = async function (
+  accepted,
+  id_professor,
+  id_application
+) {
+  try {
+    if ((accepted != null, id_professor != null, id_application != null)) {
+      let res = await applicationRepository.accRefApplication(
+        accepted,
+        id_professor,
+        id_application
+      );
     }
-  
-}
-
+    return res;
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 /**
  * Get all the application for the professor
  *
- * id_professor Integer 
+ * id_professor Integer
  * returns List
  **/
-exports.listApplication = async function(id_professor) {
-    try{
-        if(id_professor!=null){
-            console.log("SERVICE");
-            let res = await ApplicationRepository.getApplication(id_professor)
-        }
-        return res;
-
-    } catch(error){
-        res.status(500).json({ error: error.message })
+exports.listApplication = async function (id_professor) {
+  try {
+    if (id_professor != null) {
+      console.log("SERVICE");
+      let res = await applicationRepository.listApplication(id_professor);
+      return res.status(200).json();
     }
-  
-}
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
