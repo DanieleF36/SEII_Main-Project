be902678b156d6a5c59e9a9694c38ff447157b25
Francesco Pio Cellamare <s309530@studenti.polito.it>
2023-11-09 14:10:45 +0100

    some fix to findById

--- server/repositories/CoSupervisorRepository.js
+++ server/repositories/CoSupervisorRepository.js
@@ -5,15 +5,19 @@ const sqlite = require('sqlite3');
 const db = new sqlite.Database('db.sqlite', (err) => {
     if(err) throw err;
 });
-
 exports.findById = (id)=>{
-    const sqlCoSupervisor = "SELECT C.name AS name C.surname AS surname FROM CoSupervisorThesis CT, CoSupervisor C WHERE CT.id_cosupervisor = C.id AND CT.id_thesis = ?";
-    db.get(sqlCoSupervisor, [e.id], (err, row)=>{
-        if (err) {
-            reject(err);
-            return;
-        }
-        resolve({name:row.name, surname:row.surname, email:row.surname, company:row.company});
+    const sqlCoSupervisor = "SELECT name, surname, email, company FROM CoSupervisor WHERE id = ?";
+    return new Promise((resolve, reject)=>{
+        db.get(sqlCoSupervisor, [id], (err, row)=>{
+            if (err) {
+                reject(err);
+                return;
+            }
+            if(!row) 
+                resolve({})
+            else 
+                resolve({name:row.name, surname:row.surname, email:row.surname, company:row.company});
+        });
     });
 }
 
