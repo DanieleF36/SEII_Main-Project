67b0df1766586dce3852262ff6d5220628b08035
s309923 <s309923@studenti.polito.it>
2023-11-10 11:23:22 +0100

    object application

--- client/src/Components/ApplicationList.jsx
+++ client/src/Components/ApplicationList.jsx
@@ -4,32 +4,35 @@ import toast, { Toaster } from 'react-hot-toast';
 
 function ApplicationList() {
     
-    const [applications, setApplications] = useState([{id:0, 
+    const [applications, setApplications] = useState([{id_application:0, 
+                                                        id_thesis: '1',
                                                         title: 'AI system research',
-                                                        idStud: 's12345',
+                                                        id_student: 's12345',
                                                         name: 'Luca',
                                                         surname: 'Bianchi',
                                                         cds: 'A2891',
-                                                        applicationDate: '12/10/2024',
-                                                        linkCv: 'cv.pdf', 
+                                                        data: '12/10/2024',
+                                                        path_cv: 'cv.pdf', 
                                                         status: '1'},
-                                                    {id:1, 
+                                                    {id_application:1,
+                                                        id_thesis: '1', 
                                                         title: 'AI system research',
-                                                        idStud: 's25767',
+                                                        id_student: 's25767',
                                                         name: 'Aldo',
                                                         surname: 'Moro',
                                                         cds: 'A2891',
-                                                        applicationDate: '11/10/2024',
-                                                        linkCv: 'cv.pdf', 
+                                                        data: '11/10/2024',
+                                                        path_cv: 'cv.pdf', 
                                                         status: '2'},
-                                                    {id:2, 
+                                                    {id_application:2, 
+                                                        id_thesis: '2',
                                                         title: 'AI develop',
-                                                        idStud: 's25734',
+                                                        id_student: 's25734',
                                                         name: 'Aldo',
                                                         surname: 'Moro',
                                                         cds: 'A2891',
-                                                        applicationDate: '11/10/2024',
-                                                        linkCv: 'cv.pdf', 
+                                                        data: '11/10/2024',
+                                                        path_cv: 'cv.pdf', 
                                                         status: '0'}]);
     
     //adding API from backend to set list of applications
@@ -70,18 +73,18 @@ function ApplicationList() {
             <div className="flex-column rounded" style={{ backgroundColor: '#fff' }} >
             <div>
             {applications.map((application) => (
-                <Card key={application.id} style={{ marginBottom: '10px' }}>
+                <Card key={application.id_application} style={{ marginBottom: '10px' }}>
                     <Toaster position="top-center" reverseOrder={false}/>
                     <Accordion>
-                        <Accordion.Item eventKey={application.id}>
+                        <Accordion.Item eventKey={application.id_application}>
                             <Accordion.Header>
                                 <Container fluid>
                                 <Row className="d-md-flex justify-content-center align-items-center">
                                     <Col md='3' sm='3' xs='12'>
-                                        <strong>Title:</strong> {application.title}
+                                        <strong>Title:</strong> {application.title} {application.id_thesis}
                                     </Col>
                                     <Col md='3' sm='3' xs='12'>
-                                        <strong>Student:</strong> {application.idStud}
+                                        <strong>Student:</strong> {application.id_student}
                                     </Col>
                                     <Col md='5' sm='5' xs='12'>
                                         <strong>Status:</strong>{' '}
@@ -105,7 +108,7 @@ function ApplicationList() {
                                 </Container>
                             </Accordion.Header>
                             <Accordion.Body>
-                                <strong>IdStudent:</strong> {application.idStud}
+                                <strong>IdStudent:</strong> {application.id_student}
                                 <br />
                                 <strong>Name:</strong> {application.name}
                                 <br />
@@ -113,15 +116,15 @@ function ApplicationList() {
                                 <br />
                                 <strong>CdS:</strong> {application.cds}
                                 <br />
-                                <strong>Application Date:</strong> {application.applicationDate}
+                                <strong>Application Date:</strong> {application.data}
                                 <br />
-                                <strong>Link Cv: </strong> {application.linkCv}
+                                <strong>Link Cv: </strong> {application.path_cv}
                                 <br />
                                 <br />
                                 <br />
-                                {application.status === '0' ? <Button onClick={() => acceptPropByProf(application.id)} variant='primary'>Accept</Button> : ''}
+                                {application.status === '0' ? <Button onClick={() => acceptPropByProf(application.id_application)} variant='primary'>Accept</Button> : ''}
                                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                {application.status === '0' ? <Button onClick={() => rejectPropByProf(application.id)} variant='danger'>Reject</Button> : ''}
+                                {application.status === '0' ? <Button onClick={() => rejectPropByProf(application.id_application)} variant='danger'>Reject</Button> : ''}
                             </Accordion.Body>
                         </Accordion.Item>
                     </Accordion>
