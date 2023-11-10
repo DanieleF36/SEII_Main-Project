f7b3fe8aef6476a3bf90931628e51917e5de5dbd
Simone Colagiovanni <s308455@studenti.polito.it>
2023-11-10 13:21:08 +0100

    Unit test (Insert Proposal) almost working

--- server/controllers/ThesisController.js
+++ server/controllers/ThesisController.js
@@ -1,20 +1,27 @@
-'use strict';
+"use strict";
 
-const thesisService = require('../services/ThesisService');
+const thesisService = require("../services/ThesisService");
 
 function isNotConvertible(str) {
   try {
     JSON.parse(str);
     return true;
-  } catch (error) {
-  }
+  } catch (error) {}
   try {
     JSON.parse(`{${str}}`);
     return true;
-  } catch (error) {
-  }
-  const sqlKeywords = ["SELECT", "INSERT", "UPDATE", "DELETE", "FROM", "WHERE", "AND", "OR"];
-  if (sqlKeywords.some(keyword => str.includes(keyword))) {
+  } catch (error) {}
+  const sqlKeywords = [
+    "SELECT",
+    "INSERT",
+    "UPDATE",
+    "DELETE",
+    "FROM",
+    "WHERE",
+    "AND",
+    "OR",
+  ];
+  if (sqlKeywords.some((keyword) => str.includes(keyword))) {
     return true;
   }
   if (str.length >= 30) {
@@ -23,82 +30,143 @@ function isNotConvertible(str) {
   return false;
 }
 
-function checkQuery(req){
-  const orderType = ["titleD", "titleA", "supervisorD", "supervisorA", "co-supervisorD","co-supervisorA","keywordD", "keywordA", "typeD", "typeA","groupsD","groupsA","knowledgeD","knowledgeA", "expiration_dateD","expiration_dateA", "cdsD", "cdsA", "creation_dateD", "creation_dateA"]
- 
+function checkQuery(req) {
+  const orderType = [
+    "titleD",
+    "titleA",
+    "supervisorD",
+    "supervisorA",
+    "co-supervisorD",
+    "co-supervisorA",
+    "keywordD",
+    "keywordA",
+    "typeD",
+    "typeA",
+    "groupsD",
+    "groupsA",
+    "knowledgeD",
+    "knowledgeA",
+    "expiration_dateD",
+    "expiration_dateA",
+    "cdsD",
+    "cdsA",
+    "creation_dateD",
+    "creation_dateA",
+  ];
+
   if (!(req.query.page && parseInt(req.query.page) > 0)) {
-    return "Wronged Value of page "+req.query.page;
+    return "Wronged Value of page " + req.query.page;
   }
-  
+
   if (!(!req.query.order || orderType.indexOf(req.query.order) >= 0)) {
-    return "Wronged Value of order"+req.query.order
+    return "Wronged Value of order" + req.query.order;
   }
-  
-  if (!(!req.query.title || !Array.isArray(req.query.title) || !isNotConvertible(req.query.title))) {
-    return "Wronged Value of title"+req.query.title
+
+  if (
+    !(
+      !req.query.title ||
+      !Array.isArray(req.query.title) ||
+      !isNotConvertible(req.query.title)
+    )
+  ) {
+    return "Wronged Value of title" + req.query.title;
   }
-  if (!(!req.query.supervisor || !Array.isArray(req.query.supervisor) || !isNotConvertible(req.query.supervisor))) {
-    return "Wronged Value of supervisor"+req.query.supervisor
+  if (
+    !(
+      !req.query.supervisor ||
+      !Array.isArray(req.query.supervisor) ||
+      !isNotConvertible(req.query.supervisor)
+    )
+  ) {
+    return "Wronged Value of supervisor" + req.query.supervisor;
   }
-  if (req.query.coSupervisor){
-    if(!Array.isArray(req.query.coSupervisor)){
-      if( isNotConvertible(e)) {
-        return "Wronged Value of coSupervisor"+req.query.coSupervisor
-      };
-    }
-    else{
-      req.query.coSupervisor.forEach((e)=>{
-        if( isNotConvertible(e)) {
-          return "Wronged Value of coSupervisor"+req.query.coSupervisor
-        };
+  if (req.query.coSupervisor) {
+    if (!Array.isArray(req.query.coSupervisor)) {
+      if (isNotConvertible(e)) {
+        return "Wronged Value of coSupervisor" + req.query.coSupervisor;
+      }
+    } else {
+      req.query.coSupervisor.forEach((e) => {
+        if (isNotConvertible(e)) {
+          return "Wronged Value of coSupervisor" + req.query.coSupervisor;
+        }
       });
-    }  
+    }
   }
   if (req.query.keyword) {
-    if(!Array.isArray(req.query.keyword)){
-      if( isNotConvertible(req.query.keyword)) {
-        return "Wronged Value of keyword"+req.query.keyword
-      };
-    }
-    else
-      req.query.keyword.forEach((e)=>{
-        if(isNotConvertible(e)){
-          return "Wronged Value of keyword"+req.query.keyword  
-        }    
-      })
-  }
-  if (req.query.type ) {
-    if(!Array.isArray(req.query.type)){
-      if( isNotConvertible(req.query.type)) {
-        return "Wronged Value of keyword"+req.query.type
-      };
-    }
-    else
-      req.query.type.forEach((e)=>{
-        if(isNotConvertible(e)){
-          return "Wronged Value of type"+req.query.type
+    if (!Array.isArray(req.query.keyword)) {
+      if (isNotConvertible(req.query.keyword)) {
+        return "Wronged Value of keyword" + req.query.keyword;
+      }
+    } else
+      req.query.keyword.forEach((e) => {
+        if (isNotConvertible(e)) {
+          return "Wronged Value of keyword" + req.query.keyword;
         }
-      })
+      });
   }
-  
-  if (!(!req.query.groups || !Array.isArray(req.query.groups) || !isNotConvertible(req.query.groups))) {
-    return "Wronged Value of groups"+req.query.groups
+  if (req.query.type) {
+    if (!Array.isArray(req.query.type)) {
+      if (isNotConvertible(req.query.type)) {
+        return "Wronged Value of keyword" + req.query.type;
+      }
+    } else
+      req.query.type.forEach((e) => {
+        if (isNotConvertible(e)) {
+          return "Wronged Value of type" + req.query.type;
+        }
+      });
   }
-  
-  if (!(!req.query.knowledge || !Array.isArray(req.query.knowledge) || !Array.isArray(req.query.title) || !isNotConvertible(req.query.knowledge))) {
-    return "Wronged Value of knowledge"+req.query.knowledge
+
+  if (
+    !(
+      !req.query.groups ||
+      !Array.isArray(req.query.groups) ||
+      !isNotConvertible(req.query.groups)
+    )
+  ) {
+    return "Wronged Value of groups" + req.query.groups;
   }
-  
-  if (!(!req.query.expiration_date || !Array.isArray(req.query.expiration_date) || !isNotConvertible(req.query.expiration_date))) {
-    return "Wronged Value of expiration_date"+req.query.expiration_date
+
+  if (
+    !(
+      !req.query.knowledge ||
+      !Array.isArray(req.query.knowledge) ||
+      !Array.isArray(req.query.title) ||
+      !isNotConvertible(req.query.knowledge)
+    )
+  ) {
+    return "Wronged Value of knowledge" + req.query.knowledge;
   }
-  
-  if (!(!req.query.cds || !Array.isArray(req.query.cds) || !isNotConvertible(req.query.cds))) {
-    return "Wronged Value of cds"+req.query.cds
+
+  if (
+    !(
+      !req.query.expiration_date ||
+      !Array.isArray(req.query.expiration_date) ||
+      !isNotConvertible(req.query.expiration_date)
+    )
+  ) {
+    return "Wronged Value of expiration_date" + req.query.expiration_date;
   }
-  
-  if (!(!req.query.creation_date || !Array.isArray(req.query.creation_date) || !isNotConvertible(req.query.creation_date))) {
-    return "Wronged Value of creation_date"+req.query.creation_date
+
+  if (
+    !(
+      !req.query.cds ||
+      !Array.isArray(req.query.cds) ||
+      !isNotConvertible(req.query.cds)
+    )
+  ) {
+    return "Wronged Value of cds" + req.query.cds;
+  }
+
+  if (
+    !(
+      !req.query.creation_date ||
+      !Array.isArray(req.query.creation_date) ||
+      !isNotConvertible(req.query.creation_date)
+    )
+  ) {
+    return "Wronged Value of creation_date" + req.query.creation_date;
   }
   return false;
 }
@@ -114,87 +182,110 @@ function checkQuery(req){
  * - expiration_date
  * - cds
  * - creation_date
- * 
+ *
  * @returns SUCCESS: a json object as follow: {
  *  "nPage": nPage,
  *  "thesis": [ thesis1, ... ]
  * }
  * @returns ERROR: common error handling object
  */
-exports.advancedResearchThesis = function advancedResearchThesis (req, res, next) {
-  const error = checkQuery(req) 
-  if(error){
-      res.status(400).json({error:error});
-      return;
-    }
-    //checks if order is defined or not, otherwise titleD is setted as defaul value
-    const order = req.query.order?req.query.order:"titleD";
+exports.advancedResearchThesis = function advancedResearchThesis(
+  req,
+  res,
+  next
+) {
+  const error = checkQuery(req);
+  if (error) {
+    res.status(400).json({ error: error });
+    return;
+  }
+  //checks if order is defined or not, otherwise titleD is setted as defaul value
+  const order = req.query.order ? req.query.order : "titleD";
 
-    thesisService.advancedResearchThesis(req.query.page,order,req.query.title,req.query.supervisor,req.query.coSupervisor,req.query.keyword,req.query.type,req.query.groups,req.query.knowledge,req.query.expiration_date,req.query.cds,req.query.creation_date)
-      .then(function (response) {
-        let nPage = response[1];
-        response = response[0];
-        response.forEach((e)=>{
-          e.supervisor = e.supervisor.name+" "+e.supervisor.surname;
-          if(e.coSupervisors)
-            e.coSupervisors.forEach((e1, index, v) => {
-              v[index] = e1.name+" "+e1.surname;
-            });
-        });
-        res.status(200).json({"nPage":nPage, "thesis":response});
-      })
-  };
-  
-  exports.addApplication = function addApplication (req, res, next) {
-    thesisService.addApplication(req.params.id)
-      .then(function (response) {
-        res.status(201).json(response);
-      })
-      .catch(function (response) {
-
+  thesisService
+    .advancedResearchThesis(
+      req.query.page,
+      order,
+      req.query.title,
+      req.query.supervisor,
+      req.query.coSupervisor,
+      req.query.keyword,
+      req.query.type,
+      req.query.groups,
+      req.query.knowledge,
+      req.query.expiration_date,
+      req.query.cds,
+      req.query.creation_date
+    )
+    .then(function (response) {
+      let nPage = response[1];
+      response = response[0];
+      response.forEach((e) => {
+        e.supervisor = e.supervisor.name + " " + e.supervisor.surname;
+        if (e.coSupervisors)
+          e.coSupervisors.forEach((e1, index, v) => {
+            v[index] = e1.name + " " + e1.surname;
+          });
       });
-  };
-  
+      res.status(200).json({ nPage: nPage, thesis: response });
+    });
+};
+
+exports.addApplication = function addApplication(req, res, next) {
+  thesisService
+    .addApplication(req.params.id)
+    .then(function (response) {
+      res.status(201).json(response);
+    })
+    .catch(function (response) {});
+};
+
 /**
  * Wrapper function for adding a new thesis to the database
- * 
+ *
  * @param {*} req in body: thesis object
  * @param {*} res SUCCESS: thesis object | ERROR: {error: "error msg"}
- * @param {*} next 
+ * @param {*} next
  * @returns thesis object
  */
-exports.addThesis = function addThesis (req, res, next) { 
-  if(!req.body) {
-    res.status(400).json({error: "body is missing"}).send()
+exports.addThesis = function addThesis(req, res, next) {
+  if (!req.body) {
+    res.status(400).json({ error: "body is missing" }).send();
   }
 
-  if(!req.body.supervisor) {
-    res.status(400).json({error: "supervisor is missing"}).send()
+  if (!req.body.supervisor) {
+    res.status(400).json({ error: "supervisor is missing" }).send();
   }
 
-  if(!req.body.expiration_date | req.body.expiration_date == '') {
-    res.status(400).json({error: "expiration date is missing or not "}).send()
+  if (!req.body.expiration_date | (req.body.expiration_date == "")) {
+    res
+      .status(400)
+      .json({ error: "expiration date is missing or not valid" })
+      .send();
   }
 
-  if(!req.body.level || ( req.body.level != 0 && req.body.level != 1)) {
-    res.status(400).json({error: "level value not recognized"}).send()
+  if (!req.body.level || (req.body.level != 0 && req.body.level != 1)) {
+    res.status(400).json({ error: "level value not recognized" }).send();
   }
 
-  if(!req.body.status || req.body.status != 1) {
-    res.status(400).json({error: "status value not recognized or allowed"}).send()
+  if (!req.body.status || req.body.status != 1) {
+    res
+      .status(400)
+      .json({ error: "status value not recognized or allowed" })
+      .send();
   }
 
   // forcing the format
-  if(req.body.keywords) {
-    req.body.keywords = req.body.keywords.join()
+  if (req.body.keywords) {
+    req.body.keywords = req.body.keywords.join();
   }
 
-  thesisService.addThesis(req.body)
-      .then(function (response) {
-        res.status(201).json(response);
-      })
-      .catch(function (err) {
-        res.status(err.status).json({error: err})
-      });
-  };
-  
+  thesisService
+    .addThesis(req.body)
+    .then(function (response) {
+      res.status(201).json(response);
+    })
+    .catch(function (err) {
+      res.status(500).json(err);
+    });
+};
--- server/test/ThesisController.unit.test.js
+++ server/test/ThesisController.unit.test.js
@@ -1,1 +1,100 @@
 const request = require("supertest");
+const index_app = require("../index");
+const controller = require("../controllers/ThesisController");
+
+describe("/api/thesis", () => {
+  test("U1: Missing body", (done) => {
+    request(index_app)
+      .post(`/thesis`)
+      .then((response) => {
+        expect(response.status).toBe(400);
+        expect(response.error).toBeDefined();
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  test("U2: Supervisor is missing", (done) => {
+    request(index_app)
+      .post(`/thesis`)
+      .send({ expiration_date: "2052-1-1", level: 1, status: 1 })
+      .then((response) => {
+        expect(response.status).toBe(400);
+        expect(response.error).toBeDefined();
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  test("U3: Expiration date is missing", async () => {
+    request(index_app)
+      .post(`/thesis`)
+      .send({ supervisor: "Pippo", level: 1, status: 1 })
+      .then((response) => {
+        expect(response.status).toBe(400);
+        expect(response.error).toBeDefined();
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  test("U4: Level value is not recognized", async () => {
+    request(index_app)
+      .post(`/thesis`)
+      .send({ expiration_date: "2052-1-1", supervisor: "Pippo", status: 1 })
+      .then((response) => {
+        expect(response.status).toBe(400);
+        expect(response.error).toBeDefined();
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  test("U5: Status value is not recognized or allowed", async () => {
+    request(index_app)
+      .post(`/thesis`)
+      .send({ expiration_date: "2052-1-1", level: 1, supervisor: "Pippo" })
+      .then((response) => {
+        expect(response.status).toBe(400);
+        expect(response.error).toBeDefined();
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  test("U5: New thesis proposal is inserted correctly", async () => {
+    const test_thesis = [
+      {
+        title: "prova",
+        supervisor: "Pippo",
+        keywords: "prima prova",
+        type: "sperimentale",
+        groups: "back-end",
+        description: "test tesi",
+        knowledge: "test knowledge",
+        note: "test note",
+        expiration_date: "2053-1-1",
+        level: 1,
+        cds: "test cds",
+        creation_date: "2050-1-1",
+        status: 0,
+      },
+    ];
+    jest.spyOn(controller, "addThesis").mockImplementation(() => {
+      return test_thesis;
+    });
+    request(index_app)
+      .post(`/thesis`)
+      .send({
+        expiration_date: "2052-1-1",
+        level: 1,
+        status: 0,
+        supervisor: "Pippo",
+      })
+      .then((response) => {
+        expect(response.status).toBe(201);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
