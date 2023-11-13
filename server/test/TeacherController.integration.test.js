f5d90f958e15f17aa41cfb69537f08ace20be6ef
Francesco Pio Cellamare <s309530@studenti.polito.it>
2023-11-13 11:09:26 +0100

    added a sample test for Teacher controller

--- server/test/TeacherController.integration.test.js
+++ server/test/TeacherController.integration.test.js
@@ -2,3 +2,9 @@ const request = require("supertest");
 const controller = require("../controllers/TeacherController.js");
 const app = require('../index.js')
 const mgmt = require('../mgmt_db.js');
+
+describe("sample describe", () => {
+    test('sample test', () => {
+        expect(true).toBe(true)
+    })
+})
