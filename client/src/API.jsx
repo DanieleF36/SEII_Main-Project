845f3b4c55900249e70674beaa111a1c2aab73d5
Francesco Pio Cellamare <s309530@studenti.polito.it>
2023-11-15 16:49:44 +0100

    insert proposal is finally done

--- client/src/API.jsx
+++ client/src/API.jsx
@@ -2,115 +2,157 @@ import { alignPropType } from "react-bootstrap/esm/types";
 
 const URL = 'http://localhost:3001';
 
-async function listApplication(id_professor) { 
-    const response = await fetch(URL+ `/professor/${id_professor}/applications`);
-    const application = await response.json();
-    if (response.ok) {
-        console.log(application)
-       return application.map((a) => ({
-                id_application: a.id_application,
-                id_student: a.id_student,
-                id_thesis: a.id_thesis,
-                data: a.data,
-                path_cv: a.path_cv,
-                status: a.status,
-            }));
-    } else {
-      throw services;  // mi aspetto che sia un oggetto json fornito dal server che contiene l'errore
-    }
+async function listApplication(id_professor) {
+  const response = await fetch(URL + `/professor/${id_professor}/applications`);
+  const application = await response.json();
+  if (response.ok) {
+    console.log(application)
+    return application.map((a) => ({
+      id_application: a.id_application,
+      id_student: a.id_student,
+      id_thesis: a.id_thesis,
+      data: a.data,
+      path_cv: a.path_cv,
+      status: a.status,
+    }));
+  } else {
+    throw services;  // mi aspetto che sia un oggetto json fornito dal server che contiene l'errore
   }
+}
 
-async function insertProposal(thesis) {
+function insertProposal(thesis) {
     thesis.status = 1;
     thesis.cosupervisor = thesis.cosupervisor === '' ? [''] : thesis.cosupervisor
-    console.log(thesis)
-  let response = await fetch(URL + '/thesis', {
+  //   console.log(thesis)
+  // let response = await fetch(URL + '/thesis', {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(thesis)
+
+  // })
+  // let risposta = await response.json();
+  // if (risposta.status < 300 || risposta.status >= 200) {
+  //   return true;
+  // } else {
+  //   console.log('wrong')
+  //   throw response.error;
+  // }
+
+
+  return getJson(fetch(URL + '/thesis', {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(thesis)
-  
+
+  })).then(json => {
+    return json
   })
-  let risposta = await response.json();
-  if (response.ok) {
-    return true;
-  } else {
-    throw response.error;
-  }
+
 }
 
-async function advancedSearchThesis(params){
-  let ur="/thesis?";
-  if(params.page)
-    ur+="page="+params.page;
+function getJson(httpResponsePromise) {
+  // server API always return JSON, in case of error the format is the following { error: <message> } 
+  return new Promise((resolve, reject) => {
+    httpResponsePromise
+      .then((response) => {
+        if (response.ok) {
+
+          // the server always returns a JSON, even empty {}. Never null or non json, otherwise the method will fail
+          response.json()
+            .then(json => resolve(json))
+            .catch(err => reject({ error: "Cannot parse server response" }))
+
+        } else {
+          // analyzing the cause of error
+          response.json()
+            .then(obj =>
+              reject(obj)
+            ) // error msg in the response body
+            .catch(err => reject({ error: "Cannot parse server response" })) // something else
+        }
+      })
+      .catch(err =>
+        reject({ error: "Cannot communicate" })
+      ) // connection error
+  });
+}
+
+async function advancedSearchThesis(params) {
+  let ur = "/thesis?";
+  if (params.page)
+    ur += "page=" + params.page;
   else
-  ur+="page=1";
-  if(params.title)
-    ur+="&title="+params.title;
-  if(params.supervisor)
-    ur+="&supervisor="+params.supervisor;
-  if(params.cosupervisor)
-    params.cosupervisor.forEach((e, i, a)=>{
-      ur+="&coSupervisor="+a[i];
-  });
-  if(params.expDate)
-    ur+="&expiration_date="+params.expDate;
-  if(params.keywords)
-    params.keywords.forEach((e, i, a)=>{
-      ur+="&keyword="+a[i];
-  });
-  if(params.type)
-    if(params.type)
-    params.type.forEach((e, i, a)=>{
-      ur+="&type="+a[i];
-  });
-  if(params.groups)
-    ur+="&groups="+params.groups;
-  if(params.know)
-    ur+="&knowledge="+params.know;
-  if(params.cds)
-    ur+="&cds="+params.cds;
-  if(params.creatDate)
-    ur+="&creation_date="+params.creatDate;
-  if(params.ordeBy)
-    switch(ordeBy){
-      case "expDate": ur+="&order=expiration_date"+params.order;break;
-      case "title": ur+="&order=title"+params.order;break;
-      case "supervisor": ur+="&order=supervisor"+params.order;break;
+    ur += "page=1";
+  if (params.title)
+    ur += "&title=" + params.title;
+  if (params.supervisor)
+    ur += "&supervisor=" + params.supervisor;
+  if (params.cosupervisor)
+    params.cosupervisor.forEach((e, i, a) => {
+      ur += "&coSupervisor=" + a[i];
+    });
+  if (params.expDate)
+    ur += "&expiration_date=" + params.expDate;
+  if (params.keywords)
+    params.keywords.forEach((e, i, a) => {
+      ur += "&keyword=" + a[i];
+    });
+  if (params.type)
+    if (params.type)
+      params.type.forEach((e, i, a) => {
+        ur += "&type=" + a[i];
+      });
+  if (params.groups)
+    ur += "&groups=" + params.groups;
+  if (params.know)
+    ur += "&knowledge=" + params.know;
+  if (params.cds)
+    ur += "&cds=" + params.cds;
+  if (params.creatDate)
+    ur += "&creation_date=" + params.creatDate;
+  if (params.ordeBy)
+    switch (ordeBy) {
+      case "expDate": ur += "&order=expiration_date" + params.order; break;
+      case "title": ur += "&order=title" + params.order; break;
+      case "supervisor": ur += "&order=supervisor" + params.order; break;
     }
-  const response = await fetch(URL+ur);
-  if(response.status==200){
+  const response = await fetch(URL + ur);
+  if (response.status == 200) {
     const res = await response.json();
-    return [res.nPage, res.thesis.map((e)=>({
-      id:e.id,
-      title:e.title,
-      supervisor:e.supervisor,
-      co_supervisor:e.co_supervisor,
-      keyword:e.keyword ,
-      type:e.type ,
-      groups:e.groups ,
-      description:e.description ,
-      knowledge:e.knowledge ,
-      note:e.note ,
-      expiration_date:e.expiration_date ,
-      level:e.level ,
-      cds:e.cds ,
-      creation_date:e.creation_date ,
-      status:e.status, 
-      nPage:e.nPage
+    return [res.nPage, res.thesis.map((e) => ({
+      id: e.id,
+      title: e.title,
+      supervisor: e.supervisor,
+      co_supervisor: e.co_supervisor,
+      keyword: e.keyword,
+      type: e.type,
+      groups: e.groups,
+      description: e.description,
+      knowledge: e.knowledge,
+      note: e.note,
+      expiration_date: e.expiration_date,
+      level: e.level,
+      cds: e.cds,
+      creation_date: e.creation_date,
+      status: e.status,
+      nPage: e.nPage
     }))]
   }
-  else{
+  else {
     throw res;
   }
 }
 
-async function acceptApplication(status,id_professor,id_application) { 
-  const response = await fetch(URL+ `/professor/${id_professor}/applications/${id_application}`,{
-                        method: "POST",
+async function acceptApplication(status, id_professor, id_application) {
+  const response = await fetch(URL + `/professor/${id_professor}/applications/${id_application}`, {
+    method: "POST",
 
-                        body: status});              
+    body: status
+  });
   const application = await response.json();
   if (response.ok) {
     console.log("ciao");
@@ -128,29 +170,31 @@ async function acceptApplication(status,id_professor,id_application) {
   }
 }
 
-async function applyForProposal(application) { 
-    const formData = new FormData();
-    formData.append('cv', application.cv);
-    const response = await fetch(URL+ `/thesis/${application.id_thesis}/applications`,{
-        method: "POST",
-        body: formData
-    });
-    const app = await response.json();
-    if (response.status==201) {
-        return app.map((a) => ({
-                id_student: a.id_student,
-                id_thesis: a.id_thesis,
-                data: a.data,
-                path_cv: a.path_cv,
-                status: a.status,
-                id_teacher: a.id_teacher
-            ,}));
-    }
-    else if(response.status==400 || response.status==404){
-        return{error: app.error}
-    }
+
+async function applyForProposal(application) {
+  const formData = new FormData();
+  formData.append('cv', application.cv);
+  const response = await fetch(URL + `/thesis/${application.id_thesis}/applications`, {
+    method: "POST",
+    body: formData
+  });
+  const app = await response.json();
+  if (response.status == 201) {
+    return app.map((a) => ({
+      id_student: a.id_student,
+      id_thesis: a.id_thesis,
+      data: a.data,
+      path_cv: a.path_cv,
+      status: a.status,
+      id_teacher: a.id_teacher
+      ,
+    }));
+  }
+  else if (response.status == 400 || response.status == 404) {
+    return { error: app.error }
+  }
 }
 
-const API = {listApplication, insertProposal, advancedSearchThesis, acceptApplication, applyForProposal};
+const API = { listApplication, insertProposal, advancedSearchThesis, acceptApplication, applyForProposal };
 
 export default API;
