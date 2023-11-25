7e0f5632d76bcbbe6ae0ca74e37ba2cfef00d445
francineombala <131480155+francineombala@users.noreply.github.com>
2023-11-25 14:51:30 +0100

    update propposal

--- client/src/Components/MyProposal.jsx
+++ client/src/Components/MyProposal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Alert, Container, Row, Col, Dropdown, DropdownButton, Navbar, NavLink, Accordion, Badge, Card } from 'react-bootstrap';
+import { Form, Button, Alert, Container, Row, Col, Dropdown, DropdownButton, Navbar, NavLink, Accordion, Badge, Card,Modal } from 'react-bootstrap';
 import toast, { Toaster } from 'react-hot-toast';
 import API from '../API';
 import BootstrapSwitchButton from 'bootstrap-switch-button-react';
@@ -9,10 +9,27 @@ function MyProposal() {
     const [dirty, setDirty] = useState(true);
     const [id_professor, setId_professor] = useState(1);
     const [archived, setArchived] = useState(1);
+    const [selectedProposal, setSelectedProposal] = useState(null);
 
     const [proposals, setProposals] = useState([{ id: 0, title: 'AI system research', supervisor: 'Mario Rossi', cosupervisor: ['123456@polito.it', '654321@polito.it'], expDate: '10/1/2024', keywords: 'AI', type: 'Sperimental', groups: 'A32', description: 'AI thesis about...', know: 'Machine learning', level: 'Master', cds: 'LM_31', note: 'thesis for AI', creatDate: '10/1/2023', status: '1' }]);
     const [proposalsArchiv, setProposalsArchiv] = useState([{ id: 1, title: 'Prova', supervisor: 'Luca Neri', cosupervisor: ['123456@polito.it', '654321@polito.it'], expDate: '10/1/2024', keywords: 'AI', type: 'Sperimental', groups: 'A32', description: 'AI thesis about...', know: 'Machine learning', level: 'Master', cds: 'LM_31', note: 'thesis for AI', creatDate: '10/1/2023', status: '0' }]);
+    const [showModal, setShowModal] = useState(false);
+    const [proposalChanges, setProposalChanges] = useState({
+        // Initialize with default values or an empty object
 
+     title: '',
+    supervisor: '',
+    cosupervisor: '',
+    expiration_date: '',
+    keywords: '',
+    type: '',
+    groups: '',
+    description: '',
+    knowledge: '',
+    note: '',
+    level: '',
+    cds: '',
+      });
     //adding API from backend to set list of prof proposals
     /*
     useEffect(() => {
@@ -35,7 +52,71 @@ function MyProposal() {
 
     };
 
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
 
+
+    const handleModify = (proposal) => {
+        setSelectedProposal(proposal);
+        setShowModal(true);
+    };
+
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setProposalChanges((prevChanges) => ({
+          ...prevChanges,
+          [name]: value,
+        }));
+      };
+
+
+    const handleResetChange = () => {
+        setProposalData({
+            title: '',
+            supervisor: '',
+            cosupervisor: '',
+            expiration_date: '',
+            keywords: '',
+            type: '',
+            groups: '',
+            description: '',
+            knowledge: '',
+            note: '',
+            level: '',
+            cds: '',
+        });
+    };
+
+    const handleSaveChanges = () => {
+        if (proposalData.title === '') {
+            toast.error('Title field cannot be empty');
+        } else if (proposalData.keywords === '') {
+            toast.error('Keywords field cannot be empty');
+        } else if (proposalData.type === '') {
+            toast.error('Type field cannot be empty');
+        } else if (proposalData.groups === '') {
+            toast.error('Group field cannot be empty');
+        } else if (proposalData.description === '') {
+            toast.error('Description field cannot be empty');
+        } else if (proposalData.knowledge === '') {
+            toast.error('Knowledge field cannot be empty');
+        } else if (proposalData.expiration_date === '') {
+            toast.error('Expiration Date field cannot be empty');
+        } else if (proposalData.level === '') {
+            toast.error('Level field cannot be unset');
+        } else if (proposalData.cds === '') {
+            toast.error('CdS field cannot be empty');
+        } else {
+            // Implement the logic to update the proposal using the proposalData state- API
+            console.log(proposalData);
+            API.updateProposal(proposalData)
+                .then(() => { toast.success('Thesis Proposal successfully updated'); handleResetChange() })
+                .catch((error) => toast.error(error));
+        }
+    };
+    
 
     return (
         archived === 1 ? <>
@@ -94,9 +175,7 @@ function MyProposal() {
                                     <strong>Creation Date:</strong> {proposal.creatDate}
                                     <br />
                                     <br />
-                                    <Button variant="warning"> <img src="./pencil-fill.svg"
-                                                alt="info"
-                                                className="img-responsive"/></Button>
+                                    <Button variant="warning" onClick={() => handleModify(proposal)}>Modify</Button>
                                 </Accordion.Body>
                             </Accordion.Item>
                         </Accordion>
@@ -104,7 +183,162 @@ function MyProposal() {
                 ))}
             </div>
 
-        </> : <>
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Proposal</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {selectedProposal && (
+                      <Form>
+                      <Form.Group className="mb-3" controlId="formTitle">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control
+                          type="text"
+                          id="title"
+                          name="title"
+                          value={proposalChanges.title || selectedProposal.title}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>
+      
+                      <Form.Group className="mb-3" controlId="formSupervisor">
+                        <Form.Label>Supervisor</Form.Label>
+                        <Form.Control
+                          type="text"
+                          id="supervisor"
+                          name="supervisor"
+                          value={proposalChanges.supervisor || selectedProposal.supervisor}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>
+      
+                      <Form.Group className="mb-3" controlId="formCosupervisor">
+                        <Form.Label>Cosupervisor</Form.Label>
+                        <Form.Control
+                          type="text"
+                          id="cosupervisor"
+                          name="cosupervisor"
+                          value={proposalChanges.cosupervisor || selectedProposal.cosupervisor.join(', ')}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>
+      
+                      <Form.Group className="mb-3" controlId="formExpDate">
+                        <Form.Label>Expiration Date</Form.Label>
+                        <Form.Control
+                          type="text"
+                          id="expiration_date"
+                          name="expiration_date"
+                          value={proposalChanges.expiration_date || selectedProposal.expiration_date}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>
+      
+                      <Form.Group className="mb-3" controlId="formKeywords">
+                        <Form.Label>Keywords</Form.Label>
+                        <Form.Control
+                          type="text"
+                          id="keywords"
+                          name="keywords"
+                          value={proposalChanges.keywords || selectedProposal.keywords}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>
+      
+                      <Form.Group className="mb-3" controlId="formType">
+                        <Form.Label>Type</Form.Label>
+                        <Form.Control
+                          type="text"
+                          id="type"
+                          name="type"
+                          value={proposalChanges.type || selectedProposal.type}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>
+      
+                      <Form.Group className="mb-3" controlId="formGroups">
+                        <Form.Label>Groups</Form.Label>
+                        <Form.Control
+                          type="text"
+                          id="groups"
+                          name="groups"
+                          value={proposalChanges.groups || selectedProposal.groups}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>
+      
+                      <Form.Group className="mb-3" controlId="formDescription">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                          as="textarea"
+                          rows={3}
+                          id="description"
+                          name="description"
+                          value={proposalChanges.description || selectedProposal.description}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>
+      
+                      <Form.Group className="mb-3" controlId="formKnowledge">
+                        <Form.Label>Knowledge</Form.Label>
+                        <Form.Control
+                          type="text"
+                          id="knowledge"
+                          name="knowledge"
+                          value={proposalChanges.knowledge || selectedProposal.knowledge}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>
+      
+                      <Form.Group className="mb-3" controlId="formNote">
+                        <Form.Label>Note</Form.Label>
+                        <Form.Control
+                          as="textarea"
+                          rows={3}
+                          id="note"
+                          name="note"
+                          value={proposalChanges.note || selectedProposal.note}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>
+      
+                      <Form.Group className="mb-3" controlId="formLevel">
+                        <Form.Label>Level</Form.Label>
+                        <Form.Control
+                          type="text"
+                          id="level"
+                          name="level"
+                          value={proposalChanges.level || selectedProposal.level}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>
+      
+                      <Form.Group className="mb-3" controlId="formCds">
+                        <Form.Label>CdS (Course of Study)</Form.Label>
+                        <Form.Control
+                          type="text"
+                          id="cds"
+                          name="cds"
+                          value={proposalChanges.cds || selectedProposal.cds}
+                          onChange={handleInputChange}
+                        />
+                      </Form.Group>  
+                            
+                            {/* Add other form fields for editing */}
+                        </Form>
+                    )}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleSaveChanges}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+      
+</> : <>
 
             <Toaster position="top-center" reverseOrder={false} />
             <BootstrapSwitchButton onChange={() => handleSwitch()} checked={archived === 1} size="sm" onlabel='published' offlabel='archived' width={100} onstyle="success" offstyle="warning" style="border" />
@@ -162,9 +396,7 @@ function MyProposal() {
                                 <strong>Creation Date:</strong> {proposal.creatDate}
                                 <br />
                                 <br />
-                                <Button variant="warning"> <img src="./pencil-fill.svg"
-                                                alt="info"
-                                                className="img-responsive"/></Button>
+                                <Button variant="warning" onClick={() => handleModify(proposal)}>Modify</Button>
                             </Accordion.Body>
                         </Accordion.Item>
                     </Accordion>
@@ -173,9 +405,164 @@ function MyProposal() {
                 
              </div>
 
+             <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Proposal</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {selectedProposal && (
+                        
+                        <Form>
+                <Form.Group className="mb-3" controlId="formTitle">
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="title"
+                    name="title"
+                    value={proposalChanges.title || selectedProposal.title}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formSupervisor">
+                  <Form.Label>Supervisor</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="supervisor"
+                    name="supervisor"
+                    value={proposalChanges.supervisor || selectedProposal.supervisor}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formCosupervisor">
+                  <Form.Label>Cosupervisor</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="cosupervisor"
+                    name="cosupervisor"
+                    value={proposalChanges.cosupervisor || selectedProposal.cosupervisor.join(', ')}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formExpDate">
+                  <Form.Label>Expiration Date</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="expiration_date"
+                    name="expiration_date"
+                    value={proposalChanges.expiration_date || selectedProposal.expiration_date}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formKeywords">
+                  <Form.Label>Keywords</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="keywords"
+                    name="keywords"
+                    value={proposalChanges.keywords || selectedProposal.keywords}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formType">
+                  <Form.Label>Type</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="type"
+                    name="type"
+                    value={proposalChanges.type || selectedProposal.type}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formGroups">
+                  <Form.Label>Groups</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="groups"
+                    name="groups"
+                    value={proposalChanges.groups || selectedProposal.groups}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formDescription">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    id="description"
+                    name="description"
+                    value={proposalChanges.description || selectedProposal.description}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formKnowledge">
+                  <Form.Label>Knowledge</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="knowledge"
+                    name="knowledge"
+                    value={proposalChanges.knowledge || selectedProposal.knowledge}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formNote">
+                  <Form.Label>Note</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    id="note"
+                    name="note"
+                    value={proposalChanges.note || selectedProposal.note}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formLevel">
+                  <Form.Label>Level</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="level"
+                    name="level"
+                    value={proposalChanges.level || selectedProposal.level}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formCds">
+                  <Form.Label>CdS (Course of Study)</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="cds"
+                    name="cds"
+                    value={proposalChanges.cds || selectedProposal.cds}
+                    onChange={handleInputChange}
+                  />
+                </Form.Group>     
+                       
+
+                            {/* Add other form fields for editing */}
+                        </Form>
+                    )}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleSaveChanges}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
-
 }
-
+      
 export default MyProposal;
