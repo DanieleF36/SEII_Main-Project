abc837005ee6fa16aa778145a761a7b21772967a
DanieleF36 <daniele.femia@outlook.com>
2023-12-06 22:23:23 +0100

    getAllCoSupervisorsEmails and List Application tested on controller

---
+++ server/test/controller/CoSupervisorController.unit.test.js
@@ -0,0 +1,45 @@
+const controller = require("../../controllers/CoSupervisorController.js");
+let mockReq; 
+let mockRes = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+}; 
+
+beforeEach(() => {
+  mockRes = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };   
+});
+describe('getAllCoSupervisorsEmails', ()=>{
+    mockReq = { 
+        body: undefined,
+        query: undefined,
+        user: {
+          name: "Gianna",
+          lastname: "Altobella",
+          nameID: "gianni.altobelli@email.it",
+          cds: "ingInf",
+          cdsCode: "LM"
+        }  
+    };
+    test("case1: role != teacher", async()=>{
+        mockReq.user.role = 'student';
+        await controller.getAllCoSupervisorsEmails(mockReq, mockRes);
+        expect(mockRes.status).toHaveBeenCalledWith(401);
+        expect(mockRes.json).toHaveBeenCalledWith({error: "Only teacher can access to this API"});
+    }),
+    test("case2: getAllCoSupervisorsEmailsService err", async()=>{
+        mockReq.user.role = 'teacher';
+        jest.spyOn(require('../../services/CoSupervisorService'), "getAllCoSupervisorsEmailsService").mockRejectedValue({error: "error"});
+        await controller.getAllCoSupervisorsEmails(mockReq, mockRes);
+        expect(mockRes.status).toHaveBeenCalledWith(500);
+        expect(mockRes.json).toHaveBeenCalledWith({error: "Internal server error"});
+    }),
+    test("case3: getAllCoSupervisorsEmailsService success", async()=>{
+        jest.spyOn(require('../../services/CoSupervisorService.js'), "getAllCoSupervisorsEmailsService").mockResolvedValue({data: "success"});
+        await controller.getAllCoSupervisorsEmails(mockReq, mockRes);
+        expect(mockRes.status).toHaveBeenCalledWith(200);
+        expect(mockRes.json).toHaveBeenCalledWith("success");
+    })
+})
