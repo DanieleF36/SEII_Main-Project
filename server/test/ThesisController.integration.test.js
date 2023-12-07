e310188a3118dfc590cd9917b78d57726e086a8c
Francesco Pio Cellamare <s309530@studenti.polito.it>
2023-12-07 12:29:45 +0100

    fixed integration test suites for insert and update proposal

--- server/test/ThesisController.integration.test.js
+++ server/test/ThesisController.integration.test.js
@@ -5,6 +5,7 @@ const mgmt = require('../mgmt_db.js');
 const dayjs = require('dayjs')
 
 describe("INSERT PROPOSAL INTEGRATION TEST", () => {
+    let thesis
     beforeEach( async () => {
         await mgmt.cleanThesis()
         await mgmt.cleanCoSupervisor()
@@ -15,13 +16,29 @@ describe("INSERT PROPOSAL INTEGRATION TEST", () => {
             name: 'Gianni',
             lastname: 'Altobelli',
             nameID: 'gianni.altobelli@email.it',
-            role: 'teacher'
+            role: 'teacher',
+            group: 'group1'
+        }
+
+        thesis = {
+            title: "New thesis is added",
+            cosupervisor: ["gigiverdi@mail.com"],
+            keywords: ["SoftEng"],
+            type: ["abroad"],
+            groups: ["group1"],
+            description: "new thesis description",
+            knowledge: ["none"],
+            note: "none",
+            expiration_date: "2024-01-01",
+            level: "Master",
+            cds: ["ingInf"],
+            status: 1
         }
     })
 
     afterAll( async () => {
         await mgmt.cleanThesis()
-        await mgmt.cleanCoSupervisor()
+        // await mgmt.cleanCoSupervisor()
         await mgmt.cleanTeacher()
         await mgmt.cleanCoSupervisorThesis()
     })
@@ -31,7 +48,8 @@ describe("INSERT PROPOSAL INTEGRATION TEST", () => {
         await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "mariorossi@mail.com", "group1", "dep1")
         await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
 
-        const thesis = {
+
+        const expected_thesis = {
             title: "New thesis is added",
             supervisor: 1,
             cosupervisor: ["gigiverdi@mail.com"],
@@ -40,26 +58,10 @@ describe("INSERT PROPOSAL INTEGRATION TEST", () => {
             groups: ["group1"],
             description: "new thesis description",
             knowledge: ["none"],
-            note: "",
-            expiration_date: "2024-01-01",
-            level: "Master",
-            cds: ["ingInf"],
-            status: 1
-        }
-
-        const expected_thesis = {
-            title: "New thesis is added",
-            supervisor: 1,
-            cosupervisor: ["gigiverdi@mail.com"],
-            keywords: "SoftEng",
-            type: "abroad",
-            groups: "group1",
-            description: "new thesis description",
-            knowledge: "none",
-            note: "",
+            note: "none",
             expiration_date: "2024-01-01",
             level: 1,
-            cds: "ingInf",
+            cds: ["ingInf"],
             status: 1,
             creation_date: dayjs().format('YYYY-MM-DD').toString()
         }
@@ -68,30 +70,14 @@ describe("INSERT PROPOSAL INTEGRATION TEST", () => {
                 .send(thesis)
                 .expect(200)
                 .then( resp => {
-                    expect(resp.text).toEqual(JSON.stringify(expected_thesis))
+                    resp = JSON.parse(resp.text)
+                    expect(resp).toEqual(expected_thesis)
                 })
     })
 
-    test("I2: insert a thesis, a cosupervisor is missing so 500 error is returned", async () => {
+    test("I2: insert a thesis, a cosupervisor is missing so 400 error is returned", async () => {
 
         await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
-        // await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
-
-        const thesis = {
-            title: "New thesis is added",
-            supervisor: 1,
-            cosupervisor: ["gigiverdi@mail.com"],
-            keywords: ["SoftEng"],
-            type: ["abroad"],
-            groups: ["group1"],
-            description: "new thesis description",
-            knowledge: ["none"],
-            note: "",
-            expiration_date: "2024-01-01",
-            level: "Master",
-            cds: ["ingInf"],
-            status: 1
-        }
 
         await request(app)
                 .post('/thesis')
@@ -105,48 +91,19 @@ describe("INSERT PROPOSAL INTEGRATION TEST", () => {
         await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
         await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
 
-        const thesis = {
-            title: "New thesis is added",
-            supervisor: 1,
-            cosupervisor: ["gigiverdi@mail.com"],
-            keywords: ["SoftEng"],
-            type: ["abroad"],
-            groups: ["group1"],
-            description: "new thesis description",
-            knowledge: ["none"],
-            note: "",
-            expiration_date: undefined,
-            level: "Master",
-            cds: ["ingInf"],
-            status: 1
-        }
+        thesis.expiration_date = undefined
 
         await request(app)
                 .post('/thesis')
                 .send(thesis)
                 .expect(400)
-
     })
 
     test("I4: insert a thesis, keywords are not array", async () => {
         await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
         await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
 
-        const thesis = {
-            title: "New thesis is added",
-            supervisor: 1,
-            cosupervisor: ["gigiverdi@mail.com"],
-            keywords: "SoftEng",
-            type: ["abroad"],
-            groups: ["group1"],
-            description: "new thesis description",
-            knowledge: ["none"],
-            note: "",
-            expiration_date: undefined,
-            level: "Master",
-            cds: ["ingInf"],
-            status: 1
-        }
+        const thesis = 'softeng'
 
         await request(app)
                 .post('/thesis')
@@ -158,21 +115,7 @@ describe("INSERT PROPOSAL INTEGRATION TEST", () => {
         await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
         await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
 
-        const thesis = {
-            title: "New thesis is added",
-            supervisor: 1,
-            cosupervisor: "gigiverdi@mail.com",
-            keywords: ["SoftEng"],
-            type: ["abroad"],
-            groups: ["group1"],
-            description: "new thesis description",
-            knowledge: ["none"],
-            note: "",
-            expiration_date: undefined,
-            level: "Master",
-            cds: ["ingInf"],
-            status: 1
-        }
+        const thesis = "gigiverdi@mail.com"
 
         await request(app)
                 .post('/thesis')
@@ -184,21 +127,7 @@ describe("INSERT PROPOSAL INTEGRATION TEST", () => {
         await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
         await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
 
-        const thesis = {
-            title: "New thesis is added",
-            supervisor: 1,
-            cosupervisor: ["gigiverdi@mail.com"],
-            keywords: ["SoftEng"],
-            type: "abroad",
-            groups: ["group1"],
-            description: "new thesis description",
-            knowledge: ["none"],
-            note: "",
-            expiration_date: undefined,
-            level: "Master",
-            cds: ["ingInf"],
-            status: 1
-        }
+        const thesis = 'abroad'
 
         await request(app)
                 .post('/thesis')
@@ -210,21 +139,7 @@ describe("INSERT PROPOSAL INTEGRATION TEST", () => {
         await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
         await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
 
-        const thesis = {
-            title: "New thesis is added",
-            supervisor: 1,
-            cosupervisor: ["gigiverdi@mail.com"],
-            keywords: ["SoftEng"],
-            type: ["abroad"],
-            groups: "group1",
-            description: "new thesis description",
-            knowledge: ["none"],
-            note: "",
-            expiration_date: undefined,
-            level: "Master",
-            cds: ["ingInf"],
-            status: 1
-        }
+        const thesis = 'group1'
 
         await request(app)
                 .post('/thesis')
@@ -236,21 +151,7 @@ describe("INSERT PROPOSAL INTEGRATION TEST", () => {
         await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
         await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
 
-        const thesis = {
-            title: "New thesis is added",
-            supervisor: 1,
-            cosupervisor: ["gigiverdi@mail.com"],
-            keywords: ["SoftEng"],
-            type: ["abroad"],
-            groups: ["group1"],
-            description: "new thesis description",
-            knowledge: "none",
-            note: "",
-            expiration_date: undefined,
-            level: "Master",
-            cds: ["ingInf"],
-            status: 1
-        }
+        const thesis = 'none'
 
         await request(app)
                 .post('/thesis')
@@ -262,26 +163,32 @@ describe("INSERT PROPOSAL INTEGRATION TEST", () => {
         await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
         await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
 
-        const thesis = {
-            title: "New thesis is added",
-            supervisor: 1,
-            cosupervisor: ["gigiverdi@mail.com"],
-            keywords: ["SoftEng"],
-            type: ["abroad"],
-            groups: ["group1"],
-            description: "new thesis description",
-            knowledge: ["none"],
-            note: "",
-            expiration_date: undefined,
-            level: "Master",
-            cds: "ingInf",
-            status: 1
-        }
+        const thesis = 'ingInf'
 
         await request(app)
                 .post('/thesis')
                 .send(thesis)
                 .expect(400)
+    })
+
+    test("I10: insert a thesis but I'm not authorized", async () => {
+
+        await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "mariorossi@mail.com", "group1", "dep1")
+        await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
+
+        login_as.user = {
+            id: 1,
+            name: 'Gianni',
+            lastname: 'Altobelli',
+            nameID: 'gianni.altobelli@email.it',
+            role: 'student',
+            group: 'group1'
+        }
+
+        await request(app)
+                .post('/thesis')
+                .send(thesis)
+                .expect(401)
     })
 })
 
@@ -546,8 +453,165 @@ describe("SEARCH PROPOSAL INTEGRATION TEST", () => {
 
 })
 
+describe("UPDATE PROPOSAL INTEGRATION TEST", () => {
+    let thesis
+    beforeEach( async () => {
+        await mgmt.cleanThesis()
+        await mgmt.cleanCoSupervisor()
+        await mgmt.cleanTeacher()
+        await mgmt.cleanCoSupervisorThesis()        
+        
+        
+        login_as.user = {
+            id: 1,
+            name: 'Gianni',
+            lastname: 'Altobelli',
+            nameID: 'gianni.altobelli@email.it',
+            role: 'teacher',
+            group: 'group1'
+        }
 
-// describe("", () => {
-//     test('I1: get all cosupervisor emails but there are none')
+        thesis = {
+            title: "New thesis is added",
+            supervisor: 1,
+            cosupervisor: [""],
+            keywords: ["SoftEng"],
+            type: ["abroad"],
+            groups: ["group1"],
+            description: "new thesis description",
+            knowledge: ["none"],
+            note: "none",
+            expiration_date: "2024-01-01",
+            creation_date: "2023-01-01",
+            level: "Master",
+            cds: ["ingInf"],
+            status: 1
+        }
 
-// })
+        await mgmt.insertIntoThesis(1, thesis.title, thesis.supervisor, thesis.keywords, thesis.type, thesis.groups, thesis.description, thesis.knowledge, thesis.note, thesis.expiration_date, thesis.level, thesis.cds, thesis.creation_date, thesis.status)
+    })
+
+    afterAll( async () => {
+        await mgmt.cleanThesis()
+        await mgmt.cleanCoSupervisor()
+        await mgmt.cleanTeacher()
+        await mgmt.cleanCoSupervisorThesis()
+    })
+
+    test("I1: update a thesis", async () => {
+
+        await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "mariorossi@mail.com", "group1", "dep1")
+        await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
+
+        thesis.title = 'updated title'
+        await request(app)
+                .put('/thesis/1')
+                .send(thesis)
+                .expect(200)
+    })
+
+    test("I2: update a thesis, expiration date is not defined so 400 error is returned", async () => {
+
+        await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
+        await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
+
+        thesis.expiration_date = undefined
+
+        await request(app)
+                .put('/thesis/1')
+                .send(thesis)
+                .expect(400)
+    })
+
+    test("I3: update a thesis, keywords are not array", async () => {
+        await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
+        await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
+
+        const thesis = 'softeng'
+
+        await request(app)
+                .put('/thesis/1')
+                .send(thesis)
+                .expect(400)
+    })
+
+    test("I4: update a thesis, cosupervisor is not an array (even an empty one)", async () => {
+        await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
+        await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
+
+        const thesis = "gigiverdi@mail.com"
+
+        await request(app)
+                .put('/thesis/1')
+                .send(thesis)
+                .expect(400)
+    })
+
+    test("I5: update a thesis, type is not array", async () => {
+        await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
+        await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
+
+        const thesis = 'abroad'
+
+        await request(app)
+                .put('/thesis/1')
+                .send(thesis)
+                .expect(400)
+    })
+
+    test("I6: update a thesis, groups is not array", async () => {
+        await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
+        await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
+
+        const thesis = 'group1'
+
+        await request(app)
+                .put('/thesis/1')
+                .send(thesis)
+                .expect(400)
+    })
+
+    test("I7: update a thesis, knowledge is not array", async () => {
+        await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
+        await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
+
+        const thesis = 'none'
+
+        await request(app)
+                .put('/thesis/1')
+                .send(thesis)
+                .expect(400)
+    })
+
+    test("I8: update a thesis, cds is not array", async () => {
+        await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "asd@mail.com", "group1", "dep1")
+        await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
+
+        const thesis = 'ingInf'
+
+        await request(app)
+                .put('/thesis/1')
+                .send(thesis)
+                .expect(400)
+    })
+
+    test("I9: update a thesis but I'm not authorized", async () => {
+
+        await mgmt.insertIntoTeacher(1, "Rossi", "Mario", "mariorossi@mail.com", "group1", "dep1")
+        await mgmt.insertIntoCoSupervisor(1, "gigiverdi@mail.com", "Gigi", "Verdi", "Fake SRL")
+
+        login_as.user = {
+            id: 1,
+            name: 'Gianni',
+            lastname: 'Altobelli',
+            nameID: 'gianni.altobelli@email.it',
+            role: 'student',
+            group: 'group1'
+        }
+
+        await request(app)
+                .put('/thesis/1')
+                .send(thesis)
+                .expect(401)
+    })
+})
