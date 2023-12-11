23e531dce78ffd1a2393547edcd66d48f0c5b1f5
Francesco Pio Cellamare <s309530@studenti.polito.it>
2023-12-11 10:57:28 +0100

    removed a comment

--- server/index.js
+++ server/index.js
@@ -78,7 +78,7 @@ app.get("/cosupervisors/email", isLoggedIn, (req, res) => coSupervisorController
 
 app.get("/applications/student_cv/:student_id", isLoggedIn, applicationController.getStudentCv);
 
-app.get("/applications/career/:student_id", isLoggedIn, applicationController.getCareerByStudentId)//TO BE TESTED
+app.get("/applications/career/:student_id", isLoggedIn, applicationController.getCareerByStudentId)
 
 
 app.post("/testing/vc/set", (req, res) => vc.vc_set(req, res))
