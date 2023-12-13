94bb04c4fcdf3707dedee04c4a02e568724ab6f9
DanieleF36 <daniele.femia@outlook.com>
2023-12-13 13:40:21 +0100

    Solo server

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-HOSTIP=$(ping -c 1 host.docker.internal | head -n 1 | cut -d ' ' -f 3)
-echo "HOSTIP=$HOSTIP" > docker.env
-
-cd ./server
-npx nodemon index.js &
-
-cd ../client
-npm run dev
